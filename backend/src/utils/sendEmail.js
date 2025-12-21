e8c88363fbdc0f65fec464ff7c8786da18a8c663
Dhruv110904 <jaindhruv110904@gmail.com>
2025-12-21 19:33:26 +0530

    Mail update

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -9,10 +11,15 @@ const transporter = nodemailer.createTransport({
 });
 
 module.exports = async ({ to, subject, html }) => {
-  await transporter.sendMail({
-    from: `"Synaptik" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+  try {
+    await transporter.sendMail({
+      from: `"Synaptik" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+  } catch (err) {
+    console.error("EMAIL SEND ERROR:", err);
+    throw err;
+  }
 };
